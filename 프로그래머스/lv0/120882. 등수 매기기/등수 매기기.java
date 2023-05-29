c46678e5a3f1a09d7f55203bdeacb9fc49a6da6e
Jiseon <63100425+kikingki@users.noreply.github.com>
2023-05-30 00:46:45 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] solution(int[][] score) {
-        int[] answer = new int[score.length];
-        double[] average = new double[score.length];
+        int[] answer = new int[score.length];           // 등수
+        double[] average = new double[score.length];    // 평균(소수점 포함하기 위해 double 배열)
         
         for(int i = 0; i<score.length; i++) {
             average[i] = ((double)score[i][0] + score[i][1]) / 2;
             answer[i] = 1;
         }
         
+        // 점수가 더 낮을 경우에 등수를 하나씩 낮춤
         for(int i = 0; i<score.length; i++) {
             for(int j = 0; j<score.length; j++) {
                 if(average[i] < average[j]) answer[i]++;
