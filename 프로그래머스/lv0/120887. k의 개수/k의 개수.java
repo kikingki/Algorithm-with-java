c82365f92c35d860c1d33c5efaccc9dd838883bb
Jiseon <63100425+kikingki@users.noreply.github.com>
2023-06-05 04:47:07 +0900

    [level 0] Title: k의 개수, Time: 2.71 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -1,14 +1,18 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        String strK = Integer.toString(k);
         
         for(; i <= j; i++) {
-            String[] strI = Integer.toString(i).split("");
-            for(String s : strI) {
-                if(s.equals(strK)) answer += 1;
+            int n = i;
+            
+            // 자릿수마다 k가 등장하는지 확인
+            while(n > 0){
+                if(n % 10 == k) answer++;
+                n /= 10;
             }
         }
         return answer;
     }
 }
+
+// String으로 변환하는 것보다 int 타입 그대로 사용
