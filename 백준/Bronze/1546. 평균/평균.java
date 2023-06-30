b3c0582b98910c14be1a46f9048d3cf49fdfe040
Jiseon <63100425+kikingki@users.noreply.github.com>
2023-06-30 13:44:03 +0900

    [Bronze I] Title: 평균, Time: 272 ms, Memory: 20516 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        long max = 0;
+        long sum = 0;
+        
+        for(int i = 0; i < N; i++) {
+            int temp = sc.nextInt();
+            if(max < temp) max = temp;
+            sum += temp;
+        }
+            
+        // double형 계
+        System.out.println(sum*100.0/max/N);
+	}
+}
