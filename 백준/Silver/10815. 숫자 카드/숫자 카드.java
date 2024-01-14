55f3403c9db2c9e1a0f348f988abacf05730d351
Jiseon <63100425+kikingki@users.noreply.github.com>
2024-01-14 23:21:37 +0900

    [Silver V] Title: 숫자 카드, Time: 884 ms, Memory: 119664 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,20 +9,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();     // HashMap: 조회 속도가 빠름
 
         for(int i=0; i<N; i++) {
-            String card = st.nextToken();
-            map.put(card, 1);
+            map.put(st.nextToken(), 1);
         }
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
+        // StringBuilder로 출력 시간 단축
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++) {
-            String card = st.nextToken();
-            if(map.containsKey(card)) System.out.print("1 ");
-            else System.out.print("0 ");
+            if(map.containsKey(st.nextToken())) sb.append("1 ");
+            else sb.append("0 ");
         }
+        System.out.println(sb);
     }
 }
