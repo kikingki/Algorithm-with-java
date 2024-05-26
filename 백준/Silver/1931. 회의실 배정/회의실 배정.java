e7abb21b1bf415ae4a05d8a18291a0bd7cbad2af
Jiseon <63100425+kikingki@users.noreply.github.com>
2024-05-26 20:40:30 +0900

    [Silver I] Title: 회의실 배정, Time: 532 ms, Memory: 44224 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,59 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
-class Meeting implements Comparable<Meeting> {
-    int start, end;
-
-    public Meeting (int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    // 종료 시간 오름차순으로 정렬하고, 종료 시간이 같다면 시작 시간 오름차순 정렬
-    @Override
-    public int compareTo(Meeting m) {
-        if(this.end == m.end) return this.start - m.end;
-        return this.end - m.end;
-    }
-}
+import java.util.*;
 
 public class Main {
+    static class MeetingRoom implements Comparable<MeetingRoom> {
+        int startTime;
+        int endTime;
+
+        public MeetingRoom(int startTime, int endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        @Override
+        public int compareTo(MeetingRoom m) {
+            // 종료 시간이 같을 경우 시작 시간이 빠른순으로 정렬
+            if(this.endTime == m.endTime) return this.startTime - m.startTime;
+            return this.endTime - m.endTime;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         int N = Integer.parseInt(br.readLine());
-        int count = 0;      // 회의 배정 개수
 
-        ArrayList<Meeting> arr = new ArrayList<>();
+        List<MeetingRoom> meetingRoomList = new ArrayList<>();
 
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            arr.add(new Meeting(start, end));
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            meetingRoomList.add(new MeetingRoom(s, e));
         }
 
-        Collections.sort(arr);
+        Collections.sort(meetingRoomList);
 
-        int currentEnd = 0;  // 배정된 회의 종료 시간
-
+        int cnt = 0;
+        int currentEnd = 0;
         for(int i=0; i<N; i++) {
-            Meeting next = arr.get(i);
-
-            // 다음 회의 시작 시간이 배정된 회의 종료 시간보다 크거나 같다면 currentEnd 갱신
-            if (next.start >= currentEnd) {
-                count++;
-                currentEnd = next.end;
+            MeetingRoom next = meetingRoomList.get(i);
+            if(currentEnd <= next.startTime) {
+                currentEnd = next.endTime;
+                cnt++;
             }
         }
 
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
