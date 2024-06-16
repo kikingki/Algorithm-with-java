3bb25bc8a51bbfd24ac956410f523eb0aaee9442
Jiseon <63100425+kikingki@users.noreply.github.com>
2024-06-16 22:50:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 664 ms, Memory: 141684 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,56 +1,59 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
     static boolean[] visited;
-    static ArrayList<Integer>[] arr;
+    static ArrayList<Integer>[] graph;
     
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        visited = new boolean[n+1];
-        arr = new ArrayList[n+1]; // 인접리스트를 n의 개수만큼 생성
-        
-        // 인접리스트의 각 ArrayList 초기화
-        for(int i=1; i<n+1; i++) {
-            arr[i] = new ArrayList<Integer>();
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N+1];
+        graph = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) {
+            graph[i] = new ArrayList<>();
         }
-        
-        // 인접리스트에 데이터 저장
-        for(int i=0; i<m; i++) {
+
+        for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            
-            // 양방향
-            arr[s].add(e);  // 2 -> 5
-            arr[e].add(s);  // 5 -> 2
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
         }
-        
-        int count = 0;      // 연결요소의 개수
-        for(int i=1; i<n+1; i++) {
-            // 방문하지 않은 노드가 있으면
-            if(!visited[i]) { 
-                count++;
-                DFS(i);
+
+        int cnt = 0;
+        for(int i=1; i<=N; i++) {
+            if(!visited[i]) {
+                bfs(i);
+                cnt++;
             }
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
-    
-    private static void DFS(int v) {
-        // 현재 노드가 방문한 노드이면
-        if(visited[v]) { 
-            return ;
-        } else {
-            visited[v] = true;    // 현재 노드 방문 기록
 
-            // 연결 노드 중 방문하지 않은 노드
-            for(int i : arr[v]) {
-                if(!visited[i]) {
-                    DFS(i);
+    static void bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+            for(int i=0; i<graph[current].size(); i++) {
+                int next = graph[current].get(i);
+                if(!visited[next]) {
+                    visited[next] = true;
+                    q.add(next);
                 }
             }
         }
